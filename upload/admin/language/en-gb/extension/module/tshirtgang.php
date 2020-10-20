a3682316f91b281791b481e4efe71db770e4b929
andrew <andrew.lyle@tshirtgang.com>
2020-10-20 14:44:48 -0400

    Added tshirtgang API module
    - Added tshirtgang API module (previously products api module)
    - Added retrieve orders API

--- upload/admin/language/en-gb/extension/module/tshirtgang.php
+++ upload/admin/language/en-gb/extension/module/tshirtgang.php
@@ -7,7 +7,10 @@ $_['text_success']      = 'Success: API Key successfully updated!';
 $_['text_edit']         = 'Edit Tshirtgang OpenCart module';
 // Entry
 $_['entry_name']        = 'Tshirtgang API Key';
+$_['entry_title']       = 'Heading Title';
+$_['entry_status']      = 'Status';
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify the Tshirtgang module!';
 $_['error_name']        = 'Module Name must be between 3 and 64 characters!';
 $_['product_already_exists'] = "This product already exists";
+$_['version_outdated'] = "Please update to the latest version of the extension. ";
--- upload/catalog/controller/api/tshirtgang.php
+++ upload/catalog/controller/api/tshirtgang.php
@@ -1,8 +1,8 @@
 <?php
-class ControllerApiProduct extends Controller {
+class ControllerApiTshirtgang extends Controller {
 	public $version = 200; //version number
 	public function index() {
-		$this->load->language('api/product');
+		$this->load->language('api/tshirtgang');
 
 		// Delete past voucher in case there is an error
 		unset($this->session->data['product']);
@@ -12,7 +12,7 @@ class ControllerApiProduct extends Controller {
 		if (!isset($this->session->data['api_id'])) {
 			$json['error'] = $this->language->get('error_permission');
 		} else {
-			$this->load->model('extension/total/product');
+			$this->load->model('extension/total/tshirtgang');
 
 			if (isset($this->request->post['product'])) {
 				$product = $this->request->post['product'];
@@ -36,7 +36,7 @@ class ControllerApiProduct extends Controller {
 	}
 
 	public function add() {
-		$this->load->language('api/product');
+		$this->load->language('api/tshirtgang');
 
 		$json = array();
 
@@ -45,10 +45,7 @@ class ControllerApiProduct extends Controller {
 			$json['error']['warning'] = $this->language->get('error_permission');
 		} else {
 			// Add keys for missing post vars
-			$keys = array(
-				'title',
-				'description'
-			);
+			$keys = array();
 
 			foreach ($keys as $key) {
 				if (!isset($this->request->post[$key])) {
@@ -75,7 +72,7 @@ class ControllerApiProduct extends Controller {
 				unset($this->session->data['payment_method']);
 				unset($this->session->data['payment_methods']);
 			} else {
-				$Language_id = 1;
+				$Language_id = $this->config->get('config_language_id');
 				$Style_option_id = "";
 				$Color_option_id = "";
 				$Size_option_id = "";
@@ -85,7 +82,8 @@ class ControllerApiProduct extends Controller {
 				foreach ($getSKU->rows as $row) {
 					$idExists = $row['product_id'];
 					if($idExists) {
-						$json['error']['warning'] = $this->language->get('product_already_exists');
+						//echo "product already exists";
+						echo $json['error']['warning'] = $this->language->get('product_already_exists');
 						exit;
 					}
 				}
@@ -175,6 +173,12 @@ class ControllerApiProduct extends Controller {
 						language_id = '$Language_id',
 						description ='" . $this->db->escape(htmlspecialchars($this->request->post['description'])) . "'");
 					
+					//enable color options
+					$this->db->query("INSERT INTO " . DB_PREFIX . "product_option SET product_id = '$product_id',
+						option_id ='$Color_option_id',
+						required = '1'");
+					$product_option_id_color = $this->db->getLastId();
+
 					//enable size options
 					$this->db->query("INSERT INTO " . DB_PREFIX . "product_option SET product_id = '$product_id',
 						option_id ='$Size_option_id',
@@ -191,14 +195,11 @@ class ControllerApiProduct extends Controller {
 						$insert_size = $this->db->escape($this->request->post['variations'][$key]['Size']);
 						$insert_price = $this->db->escape($this->request->post['variations'][$key]['Price']);
 
-						//get list of option_value_id
-						$option_value_id_sizeArray = array();
+						//get list of option_value_id (SIZE)
 						$getattr = $this->db->query("SELECT option_value_id FROM " . DB_PREFIX . "option_value_description WHERE option_id = '$Size_option_id' AND name='$insert_size'");
 						foreach ($getattr->rows as $row) {
-							//$option_value_id_sizeArray[$row['option_value_id']] = $row['name'];
 							$product_option_description_id_size = $row['option_value_id'];
 						}
-						//if not available, insert it
 						if(!$product_option_description_id_size) {
 							$this->db->query("INSERT INTO " . DB_PREFIX . "option_value SET option_id = '$Size_option_id'");
 							$option_value_id = $this->db->getLastId();
@@ -210,7 +211,7 @@ class ControllerApiProduct extends Controller {
 							$product_option_description_id_size = $this->db->getLastId();
 						}
 
-						//insert variations
+						//insert variations - size
 						$this->db->query("INSERT INTO " . DB_PREFIX . "product_option_value SET product_id = '$product_id',
 						product_option_id ='$product_option_id_size',
 						option_id = '$Size_option_id',
@@ -243,5 +244,65 @@ class ControllerApiProduct extends Controller {
 
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
+	}
+
+	public function get() {
+		$this->load->language('api/order');
+
+		$json = array();
+
+		if (!isset($this->session->data['api_id'])) {
+			$json['error']['warning'] = $this->language->get('error_permission');
+
+		} else {
+
+			$orders = array();
+			$keys = array();
+			foreach ($keys as $key) {
+				if (!isset($this->request->post[$key])) {
+					$this->request->post[$key] = '';
+				}
+			}
+
+			$getattr = $this->db->query("SELECT oc_order.*, oc_order_product.model, oc_order_product.quantity, order_option_size.value AS SizeType, order_option_color.value AS ColorType
+			FROM `oc_order` 
+			RIGHT JOIN oc_order_product 
+				ON oc_order.order_id = oc_order_product.order_id
+			RIGHT JOIN oc_order_option AS order_option_size
+				ON oc_order_product.order_product_id = order_option_size.order_product_id AND order_option_size.name = 'Size'
+			RIGHT JOIN oc_order_option AS order_option_color
+				ON oc_order_product.order_product_id = order_option_color.order_product_id AND order_option_color.name = 'Color'
+			RIGHT JOIN oc_order_status 
+				ON oc_order.order_status_id = oc_order_status.order_status_id
+			WHERE oc_order.date_added > '".$this->db->escape($this->request->post['lastscan'])."' AND oc_order_status.name IN('Processing', 'Pending', 'Processed')
+		  	");
+			foreach ($getattr->rows as $row) {
+				array_push($orders,
+					array("order_id" => $row['order_id'],
+						"date_added" => $row['date_added'],
+						"shipping" => array("firstname" => $row['shipping_firstname'],
+											"lastname" => $row['shipping_lastname'],
+											"address1" => $row['shipping_address_1'],
+											"address2" => $row['shipping_address_2'],
+											"city" => $row['shipping_city'],
+											"state" => $row['shipping_zone'],
+											"postalcode" => $row['shipping_postcode'],
+											"country" => $row['shipping_country'],
+											"email" => $row['email'],
+											"phone" => $row['telephone']),
+						"line_items" => array("sku" => $row['model'],
+										"quantity" => $row['quantity'],
+										"style" => "na",
+										"color" => $row['ColorType'],
+										"size" => $row['SizeType'],
+										"comment" => $row['comment'])
+					));
+
+			}
+
+			$this->response->addHeader('Content-Type: application/json');
+			$this->response->setOutput(json_encode($orders));
+
+		}
 	}
 }
